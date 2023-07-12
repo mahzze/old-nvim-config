443452f2648b48a29734f0dba45b6e6257a96551
mah <zanzinzzo@gmail.com>
2023-07-12 18:38:12 -0300

    add lsp initial configs

--- lua/mah/packer.lua
+++ lua/mah/packer.lua
@@ -24,4 +24,20 @@ return require('packer').startup(function(use)
 			"nvim-lua/plenary.nvim",
 		},
 	})
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'L3MON4D3/LuaSnip'},
+			{'rafamadriz/friendly-snippets'},
+		}
+	}
 end)
